ae1c9548dc45ebcef46685f1e18e308a45bf6e7b
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-04-08 22:30:35 -0300

    user-agent

---
+++ seaching_jobs.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+url ="https://www.linkedin.com/jobs/search/?currentJobId=3550921548&distance=25&geoId=106057199&keywords=junior%20data%20analyst"
+
+headers = {User-Agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0"}
+
