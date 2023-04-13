9cbaea3ac1637de97963c9ed055f2eee4fa64394
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-04-13 10:01:51 -0300

    creating csv files

--- jbos_search/jbos_search/spiders/take_data.py
+++ jbos_search/jbos_search/spiders/take_data.py
@@ -3,8 +3,15 @@ import scrapy
 
 class TakeDataSpider(scrapy.Spider):
     name = "take_data"
-    allowed_domains = ["programathor.com.br"]
-    start_urls = ["http://programathor.com.br/"]
+    start_urls = ["https://programathor.com.br/jobs"]
 
     def parse(self, response):
-        pass 
+        for jobs in response.css('.cell-list-content'):
+            nome = jobs.css('.line-height-30 ::text').getall()
+            if nome == 'NOVA':
+                continue  # Pular para a próxima iteração do loop
+            yield {
+                'nome': nome,
+                'tecnologia': jobs.css('.cell-list-content .background-gray ::text').getall(),
+                'outrosDetalhes': jobs.css('.cell-list-content-icon span ::text').getall()
+            }
