8733f084ce50452a1e1f3e459f6124b3bca1e8ea
juedu36 <120651477+devJuliaLeal@users.noreply.github.com>
2023-04-17 13:57:03 -0300

    novos csv

--- jbos_search/jbos_search/spiders/take_data.py
+++ jbos_search/jbos_search/spiders/take_data.py
@@ -3,7 +3,7 @@ import scrapy
 
 class TakeDataSpider(scrapy.Spider):
     name = "take_data"
-    start_urls = ["https://programathor.com.br/jobs"]
+    start_urls = ["https://programathor.com.br/jobs/page/2"]
 
     def parse(self, response):
         for jobs in response.css('.cell-list-content'):
@@ -15,3 +15,9 @@ class TakeDataSpider(scrapy.Spider):
                 'tecnologia': jobs.css('.cell-list-content .background-gray ::text').getall(),
                 'outrosDetalhes': jobs.css('.cell-list-content-icon span ::text').getall()
             }
+        prox_pag = response.xpath(
+            '//*[contains(concat( " ", @class, " " ), concat( " ", "page-item", " " )) and (((count(preceding-sibling::*) + 1) = 9) and parent::*)]//*[contains(concat( " ", @class, " " ), concat( " ", "page-link", " " ))]').attrib['href']
+        if prox_pag is not None:
+            yield response.follow(prox_pag, callback=self.parse)
+            meta = {'ignore': True}
+            yield response.follow(prox_pag, meta=meta, callback=self.parse)
